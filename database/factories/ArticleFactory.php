946fb07f27d8fe829c47017afc6e22d12e6ce283
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-10 20:50:37 +0200

    article factory data added

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
- */
 class ArticleFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Article::class;
+
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'content' => fake()->paragraphs(3, true),
+            'published' => fake()->boolean(),
+            'author_id' => fake()->numberBetween(1,10),
         ];
     }
 }
