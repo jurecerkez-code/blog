789a79b61d1a8c8fa7eaba0667b0c859238149fa
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-10 21:30:08 +0200

    Enhance ArticleController by adding pagination to the index method, improving article listing performance and user experience.

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TITLE WILL COME HERE</title>
+</head>
+<body>
+    <header>
+      LOGO AND MENU WILL COME HERE
+    </header>
+    
+    <main>
+      {{ $slot }}
+    </main>
+    
+    <footer>
+      FOOTER WILL COME HERE
+    </footer>
+</body>
+</html>
