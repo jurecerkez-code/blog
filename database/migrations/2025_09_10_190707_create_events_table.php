96653d662b7c65df02360484755adb319e181b65
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-10 21:08:07 +0200

    Add detailed columns to events table migration, including title, date, location, and description

--- database/migrations/2025_09_10_190707_create_events_table.php
+++ database/migrations/2025_09_10_190707_create_events_table.php
@@ -10,12 +10,17 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('events', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('events', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->dateTime('date');
+        $table->string('location');
+        $table->text('description')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
