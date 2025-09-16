3ae876803deef57d955bbbb97fe02d28ef33159f
jurecerkez-code <jure.cerkez@code.berlin>
2025-09-16 18:46:03 +0200

    Replace email with phone in password reset tokens migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,11 +22,12 @@ return new class extends Migration
     });
 
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('phone')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
+
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
